db546c8666749691c9ca17247d87bd2c708fb2ed
Thanh Phan <thanhph111@gmail.com>
2021-07-13 23:21:23 +0700

    Add `script/setup` and `script/bootstrap`
    Read more at https://github.com/github/scripts-to-rule-them-all.

---
+++ script/setup.cmd
@@ -0,0 +1,8 @@
+@REM script/setup: Set up application for the first time after cloning, or set
+@REM               it back to the initial first unused state.
+
+@echo off
+
+cd %~dp0..
+
+.\script\bootstrap.cmd
