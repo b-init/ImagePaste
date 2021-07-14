b7e7d66f7e9850261dc99cbd5dced1aa222c6ed8
Thanh Phan <thanhph111@gmail.com>
2021-07-14 22:30:58 +0700

    Rename and reorder classes, functions, identifiers
    Also add 'Documentation' button to the add-on preferences menu and
    rewrite `bl_info`.

--- __init__.py
+++ __init__.py
@@ -1,27 +1,30 @@
 import os
 import platform
 
-import bpy
 import addon_utils
-from bpy.props import StringProperty, BoolProperty
-from bpy.types import Operator, AddonPreferences
+import bpy
+from bpy.props import BoolProperty
+from bpy.props import StringProperty
+from bpy.types import AddonPreferences
+from bpy.types import Operator
 
 
 bl_info = {
     "name": "ImagePaste",
     "author": "Binit",
-    "description": (
-        "Paste image from you clipboard as a Reference or into the Image Editor"
-    ),
     "blender": (2, 80, 0),
     "version": (1, 5, 1),
-    "location": (
-        "View3D > Add > Image, "
-        "Image Editor > Toolbar > Image, "
-        "Node Editor > Context Menu"
-    ),
-    "warning": "",
     "category": "Import-Export",
+    "doc_url": "https://github.com/Yeetus3141/ImagePaste#readme",
+    "description": (
+        "Paste images from your clipboard to "
+        "Image Editor, Shader Editor, 3D Viewport"
+    ),
+    "location": (
+        "Image Editor: Image Menu, "
+        "View3D: Add Menu > Image, "
+        "Shader Editor: Context Menu"
+    ),
 }
 
 
@@ -40,7 +43,7 @@ else:
 addon_utils.enable("io_import_images_as_planes")
 
 
-class ImagePastePreferences(AddonPreferences):
+class IMAGEPASTE_AddonPreferences(AddonPreferences):
     bl_idname = __name__
 
     default_img_dir: StringProperty(
@@ -68,132 +71,11 @@ class ImagePastePreferences(AddonPreferences):
         layout.prop(self, "force_default_dir")
 
 
-class PasteImageToImageEditor(Operator):
-    """Paste image from clipboard into the Image Editor"""
-
-    bl_idname = "impaste.paste_ie"
-    bl_label = "Paste From Clipboard"
-    bl_options = {"UNDO_GROUPED"}
-
-    def execute(self, context):
-        img_data = GrabImage()
-
-        if img_data == 0:
-            self.report({"ERROR"}, "No image data on clipboard")
-            return {"CANCELLED"}
-        elif img_data == 1:
-            self.report({"ERROR"}, "Unable to save image")
-            return {"CANCELLED"}
-        else:
-            img_dir, img_name = img_data
-
-        for directory in img_dir:
-            bpy.data.images.load(directory)
-
-        current_img = bpy.data.images[img_name[-1]]
-
-        # Set current image as active in image editor
-        for area in bpy.context.screen.areas:
-            if area.type == "IMAGE_EDITOR":
-                area.spaces.active.image = current_img
-
-        return {"FINISHED"}
-
-
-class PasteImageToReference(Operator):
-    """Load reference image from clipboard"""
-
-    bl_idname = "impaste.paste_ref"
-    bl_label = "Paste From Clipboard"
-    bl_options = {"UNDO_GROUPED"}
-
-    def execute(self, context):
-        img_data = GrabImage()
-
-        if img_data == 0:
-            self.report({"ERROR"}, "No image data on clipboard")
-            return {"CANCELLED"}
-        elif img_data == 1:
-            self.report({"ERROR"}, "Unable to save image")
-            return {"CANCELLED"}
-        else:
-            img_dir, img_name = img_data
-
-        for directory in img_dir:
-            bpy.ops.object.load_reference_image(filepath=directory)
-
-        return {"FINISHED"}
-
-
-class PasteImageAsPlane(Operator):
-    """Load image from clipboard as a plane"""
-
-    bl_idname = "impaste.paste_as_plane"
-    bl_label = "Paste From Clipboard as Plane"
-    bl_options = {"UNDO_GROUPED"}
-
-    def execute(self, context):
-        img_data = GrabImage()
-
-        if img_data == 0:
-            self.report({"ERROR"}, "No image data on clipboard")
-            return {"CANCELLED"}
-        elif img_data == 1:
-            self.report({"ERROR"}, "Unable to save image")
-            return {"CANCELLED"}
-        else:
-            img_dir, img_name = img_data
-
-        for directory in img_dir:
-            name = os.path.basename(directory)
-            path = os.path.dirname(directory)
-
-            bpy.ops.import_image.to_plane(
-                files=[{"name": name, "name": name}], directory=path, relative=False
-            )
-
-        return {"FINISHED"}
-
-
-class PasteImageToNodeEditor(Operator):
-    """Paste image(s) from clipboard as image texture node(s)"""
-
-    bl_idname = "impaste.paste_as_node"
-    bl_label = "Paste Images From Clipboard"
-    bl_options = {"UNDO_GROUPED"}
-
-    def execute(self, context):
-        img_data = GrabImage()
-
-        if img_data == 0:
-            self.report({"ERROR"}, "No image data on clipboard")
-            return {"CANCELLED"}
-        elif img_data == 1:
-            self.report({"ERROR"}, "Unable to save image")
-            return {"CANCELLED"}
-        else:
-            img_dir, _ = img_data
-
-        tree = context.space_data.edit_tree
-        locX, locY = context.space_data.cursor_location
-
-        for directory in img_dir:
-            node = tree.nodes.new("ShaderNodeTexImage")
-            node.location = locX, locY
-            # Offset location for next node
-            locY += 300
-
-            node_img = bpy.data.images.load(filepath=directory)
-            node.image = node_img
-
-        return {"FINISHED"}
-
-
-class CopyImageToClipboard(Operator):
+class IMAGEPASTE_OT_imageeditor_copy(Operator):
     """Copy image to clipboard"""
 
-    bl_idname = "impaste.copy_img"
-    bl_label = "Copy To Clipboard"
+    bl_idname = "imagepaste.copy_imageeditor"
+    bl_label = "Copy to Clipboard"
     bl_options = {"UNDO_GROUPED"}
 
     def execute(self, context):
@@ -235,35 +117,171 @@ class CopyImageToClipboard(Operator):
         return {"FINISHED"}
 
 
+class IMAGEPASTE_OT_imageeditor_paste(Operator):
+    """Paste image from clipboard into the Image Editor"""
+
+    bl_idname = "imagepaste.paste_imageeditor"
+    bl_label = "Paste from Clipboard"
+    bl_options = {"UNDO_GROUPED"}
+
+    def execute(self, context):
+        img_data = GrabImage()
+
+        if img_data == 0:
+            self.report({"ERROR"}, "No image data on clipboard")
+            return {"CANCELLED"}
+        elif img_data == 1:
+            self.report({"ERROR"}, "Unable to save image")
+            return {"CANCELLED"}
+        else:
+            img_dir, img_name = img_data
+
+        for directory in img_dir:
+            bpy.data.images.load(directory)
+
+        current_img = bpy.data.images[img_name[-1]]
+
+        # Set current image as active in image editor
+        for area in bpy.context.screen.areas:
+            if area.type == "IMAGE_EDITOR":
+                area.spaces.active.image = current_img
+
+        return {"FINISHED"}
+
+
+class IMAGEPASTE_OT_shadereditor_paste(Operator):
+    """Paste image(s) from clipboard as image texture node(s)"""
+
+    bl_idname = "imagepaste.paste_shadereditor"
+    bl_label = "Paste from Clipboard"
+    bl_options = {"UNDO_GROUPED"}
+
+    def execute(self, context):
+        img_data = GrabImage()
+
+        if img_data == 0:
+            self.report({"ERROR"}, "No image data on clipboard")
+            return {"CANCELLED"}
+        elif img_data == 1:
+            self.report({"ERROR"}, "Unable to save image")
+            return {"CANCELLED"}
+        else:
+            img_dir, _ = img_data
+
+        tree = context.space_data.edit_tree
+        locX, locY = context.space_data.cursor_location
+
+        for directory in img_dir:
+            node = tree.nodes.new("ShaderNodeTexImage")
+            node.location = locX, locY
+            # Offset location for next node
+            locY += 300
+
+            node_img = bpy.data.images.load(filepath=directory)
+            node.image = node_img
+
+        return {"FINISHED"}
+
+
+class IMAGEPASTE_OT_view3d_paste_plane(Operator):
+    """Load image from clipboard as a plane"""
+
+    bl_idname = "imagepaste.paste_view3d_plane"
+    bl_label = "Paste from Clipboard as Plane"
+    bl_options = {"UNDO_GROUPED"}
+
+    def execute(self, context):
+        img_data = GrabImage()
+
+        if img_data == 0:
+            self.report({"ERROR"}, "No image data on clipboard")
+            return {"CANCELLED"}
+        elif img_data == 1:
+            self.report({"ERROR"}, "Unable to save image")
+            return {"CANCELLED"}
+        else:
+            img_dir, img_name = img_data
+
+        for directory in img_dir:
+            name = os.path.basename(directory)
+            path = os.path.dirname(directory)
+
+            bpy.ops.import_image.to_plane(
+                files=[{"name": name, "name": name}], directory=path, relative=False
+            )
+
+        return {"FINISHED"}
+
+
+class IMAGEPASTE_OT_view3d_paste_reference(Operator):
+    """Load reference image from clipboard"""
+
+    bl_idname = "imagepaste.paste_view3d_reference"
+    bl_label = "Paste from Clipboard as Reference"
+    bl_options = {"UNDO_GROUPED"}
+
+    def execute(self, context):
+        img_data = GrabImage()
+
+        if img_data == 0:
+            self.report({"ERROR"}, "No image data on clipboard")
+            return {"CANCELLED"}
+        elif img_data == 1:
+            self.report({"ERROR"}, "Unable to save image")
+            return {"CANCELLED"}
+        else:
+            img_dir, img_name = img_data
+
+        for directory in img_dir:
+            bpy.ops.object.load_reference_image(filepath=directory)
+
+        return {"FINISHED"}
+
+
 # Menu functions
-def menu_func_ie(self, context):
-    self.layout.operator(PasteImageToImageEditor.bl_idname, icon="FILE_IMAGE")
+def imageeditor_copy_imagemenu_draw(self, context):
+    self.layout.operator(
+        IMAGEPASTE_OT_imageeditor_copy.bl_idname,
+        icon="COPYDOWN",
+    )
 
 
-def menu_func_ref(self, context):
-    self.layout.operator(PasteImageToReference.bl_idname, icon="FILE_IMAGE")
+def imageeditor_paste_imagemenu_draw(self, context):
+    self.layout.operator(
+        IMAGEPASTE_OT_imageeditor_paste.bl_idname,
+        icon="FILE_IMAGE",
+    )
 
 
-def menu_func_as_plane(self, context):
-    self.layout.operator(PasteImageAsPlane.bl_idname, icon="IMAGE_PLANE")
+def shadereditor_paste_contextmenu_draw(self, context):
+    self.layout.operator(
+        IMAGEPASTE_OT_shadereditor_paste.bl_idname,
+        icon="FILE_IMAGE",
+    )
 
 
-def menu_func_as_node(self, context):
-    self.layout.operator(PasteImageToNodeEditor.bl_idname, icon="FILE_IMAGE")
+def view3d_paste_plane_imageaddmenu_draw(self, context):
+    self.layout.operator(
+        IMAGEPASTE_OT_view3d_paste_plane.bl_idname,
+        icon="IMAGE_PLANE",
+    )
 
 
-def menu_func_to_clipboard(self, context):
-    self.layout.operator(CopyImageToClipboard.bl_idname, icon="COPYDOWN")
+def view3d_paste_reference_imageaddmenu_draw(self, context):
+    self.layout.operator(
+        IMAGEPASTE_OT_view3d_paste_reference.bl_idname,
+        icon="FILE_IMAGE",
+    )
 
 
 # List of all classes to register/unregister
 classes = (
-    PasteImageToReference,
-    PasteImageAsPlane,
-    PasteImageToImageEditor,
-    ImagePastePreferences,
-    PasteImageToNodeEditor,
-    CopyImageToClipboard,
+    IMAGEPASTE_AddonPreferences,
+    IMAGEPASTE_OT_imageeditor_copy,
+    IMAGEPASTE_OT_imageeditor_paste,
+    IMAGEPASTE_OT_shadereditor_paste,
+    IMAGEPASTE_OT_view3d_paste_plane,
+    IMAGEPASTE_OT_view3d_paste_reference,
 )
 
 # Store keymaps here to access after registration
@@ -278,11 +296,11 @@ def register():
         bpy.utils.register_class(current)
 
     # Register menus
-    bpy.types.IMAGE_MT_image.append(menu_func_ie)
-    bpy.types.VIEW3D_MT_image_add.append(menu_func_ref)
-    bpy.types.VIEW3D_MT_image_add.append(menu_func_as_plane)
-    bpy.types.NODE_MT_context_menu.append(menu_func_as_node)
-    bpy.types.IMAGE_MT_image.append(menu_func_to_clipboard)
+    bpy.types.IMAGE_MT_image.append(imageeditor_copy_imagemenu_draw)
+    bpy.types.IMAGE_MT_image.append(imageeditor_paste_imagemenu_draw)
+    bpy.types.NODE_MT_context_menu.append(shadereditor_paste_contextmenu_draw)
+    bpy.types.VIEW3D_MT_image_add.append(view3d_paste_plane_imageaddmenu_draw)
+    bpy.types.VIEW3D_MT_image_add.append(view3d_paste_reference_imageaddmenu_draw)
 
     # Register keymaps
     wm = bpy.context.window_manager
@@ -290,7 +308,7 @@ def register():
     if kc:
         km = kc.keymaps.new(name="Image Generic", space_type="IMAGE_EDITOR")
         kmi = km.keymap_items.new(
-            PasteImageToImageEditor.bl_idname,
+            IMAGEPASTE_OT_imageeditor_paste.bl_idname,
             type="V",
             value="PRESS",
             ctrl=True,
@@ -300,7 +318,7 @@ def register():
 
         km = kc.keymaps.new(name="3D View", space_type="VIEW_3D")
         kmi = km.keymap_items.new(
-            PasteImageToReference.bl_idname,
+            IMAGEPASTE_OT_view3d_paste_reference.bl_idname,
             type="V",
             value="PRESS",
             ctrl=True,
@@ -310,7 +328,7 @@ def register():
 
         km = kc.keymaps.new(name="3D View", space_type="VIEW_3D")
         kmi = km.keymap_items.new(
-            PasteImageAsPlane.bl_idname,
+            IMAGEPASTE_OT_view3d_paste_plane.bl_idname,
             type="V",
             value="PRESS",
             ctrl=True,
@@ -321,7 +339,7 @@ def register():
 
         km = kc.keymaps.new(name="Node Editor", space_type="NODE_EDITOR")
         kmi = km.keymap_items.new(
-            PasteImageToNodeEditor.bl_idname,
+            IMAGEPASTE_OT_shadereditor_paste.bl_idname,
             type="V",
             value="PRESS",
             ctrl=True,
@@ -331,7 +349,7 @@ def register():
 
         km = kc.keymaps.new(name="Image Generic", space_type="IMAGE_EDITOR")
         kmi = km.keymap_items.new(
-            CopyImageToClipboard.bl_idname,
+            IMAGEPASTE_OT_imageeditor_copy.bl_idname,
             type="C",
             value="PRESS",
             ctrl=True,
@@ -346,18 +364,14 @@ def unregister():
         bpy.utils.unregister_class(current)
 
     # Unregister menus
-    bpy.types.IMAGE_MT_image.remove(menu_func_ie)
-    bpy.types.VIEW3D_MT_image_add.remove(menu_func_ref)
-    bpy.types.VIEW3D_MT_image_add.remove(menu_func_as_plane)
-    bpy.types.NODE_MT_context_menu.remove(menu_func_as_node)
-    bpy.types.IMAGE_MT_image.remove(menu_func_to_clipboard)
+    bpy.types.VIEW3D_MT_image_add.remove(view3d_paste_reference_imageaddmenu_draw)
+    bpy.types.VIEW3D_MT_image_add.remove(view3d_paste_plane_imageaddmenu_draw)
+    bpy.types.NODE_MT_context_menu.remove(shadereditor_paste_contextmenu_draw)
+    bpy.types.IMAGE_MT_image.remove(imageeditor_paste_imagemenu_draw)
+    bpy.types.IMAGE_MT_image.remove(imageeditor_copy_imagemenu_draw)
 
     # Unregister keymaps
     if kc:
         for km, kmi in addon_keymaps:
             km.keymap_items.remove(kmi)
         addon_keymaps.clear()
-
-
-if __name__ == "__main__":
-    register()
