c8b6b1a1db1155a36aac7401b4837897d3bfe607
Thanh Phan <thanhph111@gmail.com>
2021-07-29 16:43:17 +0700

    Fix the logic of getting the saved directory
    The directory returned should follow this table:
    
    | bpy       | is_use   | is_force_use | another | Return directory |
    | .data     | _another | _another_dir | _dir    |                  |
    | .filepath | _dir     |              |         |                  |
    | --------- | ---------| ------------ | --------| ---------------  |
    | FALSE     | FALSE    | FALSE        | FALSE   | bpy.app.tempdir  |
    | FALSE     | FALSE    | FALSE        | TRUE    | bpy.app.tempdir  |
    | FALSE     | FALSE    | TRUE         | FALSE   | bpy.app.tempdir  |
    | FALSE     | FALSE    | TRUE         | TRUE    | bpy.app.tempdir  |
    | FALSE     | TRUE     | FALSE        | FALSE   | bpy.app.tempdir  |
    | FALSE     | TRUE     | FALSE        | TRUE    | another_dir      |
    | FALSE     | TRUE     | TRUE         | FALSE   | bpy.app.tempdir  |
    | FALSE     | TRUE     | TRUE         | TRUE    | another_dir      |
    | TRUE      | FALSE    | FALSE        | FALSE   | .blend directory |
    | TRUE      | FALSE    | FALSE        | TRUE    | .blend directory |
    | TRUE      | FALSE    | TRUE         | FALSE   | .blend directory |
    | TRUE      | FALSE    | TRUE         | TRUE    | .blend directory |
    | TRUE      | TRUE     | FALSE        | FALSE   | .blend directory |
    | TRUE      | TRUE     | FALSE        | TRUE    | .blend directory |
    | TRUE      | TRUE     | TRUE         | FALSE   | .blend directory |
    | TRUE      | TRUE     | TRUE         | TRUE    | another_dir      |
    
    * `dir` is a short for `directory`

--- imagepaste/helper.py
+++ imagepaste/helper.py
@@ -10,8 +10,6 @@ def get_addon_preferences() -> bpy.types.AddonPreferences:
     Returns:
         bpy.types.AddonPreferences: The addon preferences.
     """
-    import bpy
-
     return bpy.context.preferences.addons[ADDON_NAME].preferences
 
 
@@ -28,14 +26,22 @@ def get_save_directory() -> str:
 
     preferences = get_addon_preferences()
 
-    if bpy.data.filepath and not preferences.is_force_use_another_directory:
-        directory_path = dirname(bpy.data.filepath)
-        if preferences.is_use_subdirectory and preferences.subdirectory_name:
-            subdirectory_path = join(directory_path, preferences.subdirectory_name)
-            if not exists(subdirectory_path):
-                makedirs(subdirectory_path)
-            return subdirectory_path
-        return directory_path
-    if preferences.is_use_another_directory and preferences.another_directory:
+    if not bpy.data.filepath:
+        if preferences.is_use_another_directory and preferences.another_directory:
+            return preferences.another_directory
+        return bpy.app.tempdir
+
+    if (
+        preferences.is_use_another_directory
+        and preferences.is_force_use_another_directory
+        and preferences.another_directory
+    ):
         return preferences.another_directory
-    return bpy.app.tempdir
+
+    directory_path = dirname(bpy.data.filepath)
+    if not preferences.is_use_subdirectory or not preferences.subdirectory_name:
+        return directory_path
+    subdirectory_path = join(directory_path, preferences.subdirectory_name)
+    if not exists(subdirectory_path):
+        makedirs(subdirectory_path)
+    return subdirectory_path
