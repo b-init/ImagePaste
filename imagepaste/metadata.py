c136dc08066ee4ffd3a01a67f1dee70f3ee2d7e1
Thanh Phan <thanhph111@gmail.com>
2021-08-30 21:06:32 +0700

    Add function to get `bl_info` for future use
    It's taken from the closed pull request #19.

--- imagepaste/metadata.py
+++ imagepaste/metadata.py
@@ -11,3 +11,14 @@ def get_addon_preferences() -> bpy.types.AddonPreferences:
         bpy.types.AddonPreferences: The addon preferences.
     """
     return bpy.context.preferences.addons[ADDON_NAME].preferences
+
+
+def get_addon_bl_info() -> dict:
+    """Get add-on bl_info defined in __init__.py.
+
+    Returns:
+        dict: a dictionary containing add-on info.
+    """
+    from importlib import import_module
+
+    return import_module(ADDON_NAME).bl_info
