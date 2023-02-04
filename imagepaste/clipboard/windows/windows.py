f1dea5702c71c7d3790a7aad46ec95c8ce7d36a3
Binit <binitnew@gmail.com>
2023-02-04 15:40:53 +0530

    updated clipette.py to prevent improper exit
    to prevent exiting without closing clipboard

--- imagepaste/clipboard/windows/windows.py
+++ imagepaste/clipboard/windows/windows.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
+from posixpath import abspath
 
 from ..clipboard import Clipboard
 from ...report import Report
 from ...image import Image
+from ...process import Process
 
 
 class WindowsClipboard(Clipboard):
     """A concrete implementation of Clipboard for Windows."""
 
     def __init__(self, report: Report, images: list[Image] = None) -> None:
-        """A concrete implementation of Clipboard for Windows.
+        """A concreate implementation of Clipboard for Windows.
 
         Args:
             report (Report): A Report instance to which results should be reported.
@@ -35,30 +37,33 @@ class WindowsClipboard(Clipboard):
         filename = cls.get_filename()
         filepath = join(save_directory, filename)
 
-        clipette.open_clipboard()
+        if clipette.open_clipboard():
 
-        # load multiple images first if filepaths are available (as CF_HDROP, id 15)
-        if clipette.is_format_available(15):
-            filepaths = clipette.get_FILEPATHS()
+            # load multiple images first if filepaths are available (as CF_HDROP, id 15)
+            if clipette.is_format_available(15):
+                filepaths = clipette.get_FILEPATHS()
+                clipette.close_clipboard()
+
+                images = [Image(filepath) for filepath in filepaths]
+                return cls(Report(6, f"Pasted {len(images)} image files: {images}"), images)
+
+            # get image if available as 'PNG' or 'image/png' which covers pretty much all software.
+            # Ditched BITMAP support because blender doesn't completely support all Bitmap sub-formats
+            # and I couldn't find any software that copies only as a bitmap.
+            output = clipette.get_PNG(save_directory, splitext(filename)[0])
             clipette.close_clipboard()
 
-            images = [Image(filepath) for filepath in filepaths]
-            return cls(Report(6, f"Pasted {len(images)} image files: {images}"), images)
+            
+            if output != 1:
+                image = Image(filepath)
+                return cls(Report(3, f"Cannot save image: {image}"))
+            else:
+                image = Image(filepath, pasted=True)
+                return cls(Report(6, f"Saved and pasted 1 image: {image}"), [image])
 
-        # Get image if available as 'PNG' or 'image/png' which covers pretty much all
-        # software. Ditched BITMAP support because blender doesn't completely support
-        # all Bitmap sub-formats and I couldn't find any software that copies only as a
-        # bitmap.
-        output = clipette.get_PNG(save_directory, splitext(filename)[0])
-        clipette.close_clipboard()
-        if output != 1:
-            image = Image(filepath)
-            return cls(Report(3, f"Cannot save image: {image}"))
         else:
-            image = Image(filepath, pasted=True)
-            return cls(Report(6, f"Saved and pasted 1 image: {image}"), [image])
-
-        return cls(Report(2))
+            image = Image(filepath)
+            return cls(Report(1, "Unable to access clipboard"))
 
     @classmethod
     def pull(cls, image_path: str) -> WindowsClipboard:
@@ -72,66 +77,65 @@ class WindowsClipboard(Clipboard):
                 operations under Report object and a list of one Image object that holds
                 information of the pulled image we put its path to the input.
         """
-        from . import clipette
+        from . import clipette 
         from bpy.path import abspath
 
         clipette.open_clipboard()
-        clipette.empty_clipboard()
-
+        clipette.empty_cliboard()
+        
         image_path = abspath(image_path)
         image_format = image_path[-3:].lower()
-        # bmp (as DIB, DIBV5, BITMAP) and png (as PNG) should be enough formats to work
-        # with most applications
-        if image_format != "bmp":
-            clipette.set_DIB(cls.convert_image(image_path, "BMP"))
+        # bmp (as DIB, DIBV5, BITMAP) and png (as PNG) should be enough formats to work with most applications
+        if image_format != 'bmp':
+            clipette.set_DIB(cls.convert_image(image_path, 'BMP'))
         else:
             clipette.set_DIB(image_path)
 
-        if image_format != "png":
-            clipette.set_PNG(cls.convert_image(image_path, "PNG"))
+        if image_format != 'png':
+            clipette.set_PNG(cls.convert_image(image_path, 'PNG'))
         else:
             clipette.set_PNG(image_path)
-
+            
         clipette.close_clipboard()
 
         image = Image(image_path)
         return cls(Report(5, f"Copied 1 image: {image}"), [image])
 
+
     @staticmethod
     def convert_image(image_path: str, format: str) -> str:
-        """A static method to convert image format and get new image filepath.
+        """A static method to convert image format and get new image filepath. 
         Saves converted image in ImagePaste's working directory.
 
         Args:
             image_path (str): Filepath of source image.
-            format (str): Format to convert image to as in the image extension ('png',
-                'bmp', etc)
+            format (str): Format to convert image to as in the image extension ('png', 'bmp', etc)
 
         Returns:
             str: Filepath of converted image.
         """
-        # should probably incorporate this function into the Image class or something
+        # should probably incorpoate this function into the Image class or something
         from ...tree import get_save_directory
         from os.path import join, basename, splitext
         from bpy_extras.image_utils import load_image
         import bpy
-
-        RGBA_unsupported = ["BMP", "JPEG"]
+        
+        RGBA_unsupported = ['BMP', 'JPEG']
         format_ext = {
-            "BMP": ".bmp",
-            "IRIS": ".rgb",
-            "PNG": ".png",
-            "JPEG": ".jpg",
-            "JPEG2000": ".jp2",
-            "TARGA": ".tga",
-            "TARGA_RAW": ".tga",
-            "CINEON": ".cin",
-            "DPX": ".dpx",
-            "OPEN_EXR_MULTILAYER": ".exr",
-            "OPEN_EXR": ".exr",
-            "HDR": ".hdr",
-            "TIFF": ".tif",
-            "WEBP": ".webp",
+            'BMP': '.bmp',
+            'IRIS': '.rgb',
+            'PNG': '.png',
+            'JPEG': '.jpg',
+            'JPEG2000': '.jp2',
+            'TARGA': '.tga',
+            'TARGA_RAW': '.tga',
+            'CINEON': '.cin',
+            'DPX': '.dpx',
+            'OPEN_EXR_MULTILAYER': '.exr',
+            'OPEN_EXR': '.exr',
+            'HDR': '.hdr',
+            'TIFF': '.tif',
+            'WEBP': '.webp'
         }
 
         img_settings = bpy.context.scene.render.image_settings
@@ -141,12 +145,10 @@ class WindowsClipboard(Clipboard):
 
         img_settings.file_format = format
         img_settings.quality = 100
-        img_settings.color_mode = "RGB" if format in RGBA_unsupported else "RGBA"
+        img_settings.color_mode = 'RGB' if format in RGBA_unsupported else 'RGBA'
 
         image = load_image(image_path)
-        image_path_c = join(
-            get_save_directory(), splitext(basename(image_path))[0] + format_ext[format]
-        )
+        image_path_c = join(get_save_directory(), splitext(basename(image_path))[0] + format_ext[format])
         image.save_render(image_path_c)
 
         img_settings.file_format = prev_file_format
